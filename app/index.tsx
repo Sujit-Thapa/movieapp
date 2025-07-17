b54ec3f139deb9c05d3439122cdbc57e33d9571e
Sujit-Thapa <sujitthapa.professional@gmail.com>
2025-07-17 16:32:09 +0545

    app init

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+ 
+export default function index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text >
+        Welcome to Nativewind!
+      </Text>
+    </View>
+  );
+}
